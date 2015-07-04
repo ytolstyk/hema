368f0d8adbeb2ef8dbc169dc181840da601226e7
azizoval <azizoval@azizoval-macbookpro.roam.corp.google.com>
2015-07-04 10:32:01 -0700

    annotate

--- app/models/pool_fighter.rb
+++ app/models/pool_fighter.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: pool_fighters
+#
+#  id         :integer          not null, primary key
+#  pool_id    :integer          not null
+#  fighter_id :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class PoolFighter < ActiveRecord::Base
 end
--- test/models/pool_fighter_test.rb
+++ test/models/pool_fighter_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: pool_fighters
+#
+#  id         :integer          not null, primary key
+#  pool_id    :integer          not null
+#  fighter_id :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class PoolFighterTest < ActiveSupport::TestCase
