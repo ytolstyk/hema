8f0ffc764290c41be1f96f5b5aa3ee35c3462584
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2015-07-03 22:49:25 -0700

    users and sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,55 +16,17 @@ Rails.application.routes.draw do
   scope 'fighters', controller: 'fighters' do
     get ':id' => :show, as: :fighters_show
   end
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  scope 'users', controller: 'users' do
+    get 'index' => :index, as: :users_index
+    get 'new' => :new, as: :users_new
+    post 'create' => :create, as: :users_create
+    get 'show' => :show, as: :users_show
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  scope 'sessions', controller: 'sessions' do
+    get 'new' => :new, as: :sessions_new
+    post 'create' => :create, as: :sessions_create
+    delete 'destroy' => :destroy, as: :sessions_destroy
+  end
 end
