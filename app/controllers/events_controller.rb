4091003a67173450efab7d837f8a1c93b2706c50
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2015-07-10 22:42:44 -0700

    create and destroy events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,31 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     @tournaments = @event.tournaments
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to events_index_path
+    else
+      flash.now[:errors] = @event.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_index_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date, :organizer)
+  end
 end
