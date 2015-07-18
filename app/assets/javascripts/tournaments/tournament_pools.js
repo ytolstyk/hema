6d39db18077f87374d5866a1224f50250b9badb9
azizoval <azizoval@azizoval-macbookpro.roam.corp.google.com>
2015-07-18 13:35:45 -0700

    Add sortable to pool fighters

---
+++ app/assets/javascripts/tournaments/tournament_pools.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.tournament-pool-fighters-sortable').sortable({
+      connectWith: '.tournament-pool-fighters-sortable'
+    });
+    $('.tournament-pool-fighters-sortable').disableSelection();
+});
