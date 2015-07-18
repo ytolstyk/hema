04b79ceb291a7e95bc51a41bbe40d5d77ae40ba3
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2015-07-17 21:20:51 -0700

    removing comments

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -5,10 +5,6 @@ describe Tournament do
     it "should validate empty fields" do
       tournament = Tournament.create
       tournament.errors.full_messages.length.should > 0
-      # expect { Tournament.create }.to raise_error
-      # expect { Tournament.create(name: 'test') }.to raise_error
-      # expect { Tournament.create(name: 'test', event_id: 1) }.to raise_error
-      # expect { Tournament.create(name: 'test', event_id: 1, weapon_type: 'wood') }.to be_valid
     end
 
     it "should validate empty fields" do
