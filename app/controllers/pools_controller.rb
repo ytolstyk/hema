2f9d3e61dfd3548ceecb4c13c3366958da1db1b2
azizoval <azizoval@azizoval-macbookpro.roam.corp.google.com>
2015-07-19 21:06:00 -0700

    Rename other pools on delete, so naming is continuous

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -2,8 +2,8 @@ class PoolsController < ApplicationController
   before_action :ensure_logged_in, only: [:create, :destroy]
 
   def create
-    name = "Pool #{Tournament.find(pool_params[:tournament_id]).pools.count + 1}"
-    @pool = Pool.create(name: name, tournament_id: pool_params[:tournament_id])
+    @pool = Pool.create(name: 'New pool', tournament_id: pool_params[:tournament_id])
+    @pool.name_pool Tournament.find(pool_params[:tournament_id]).pools.count - 1
     if !@pool.save
       flash[:errors] = @pool.errors.full_messages
     end
@@ -18,8 +18,7 @@ class PoolsController < ApplicationController
       return
     end
 
-    @pool.reassign_fighters Pool::DEFAULT_POOL
-    @pool.destroy
+    @pool.remove_pool
     redirect_to tournament_pools_path(@pool.tournament_id)
   end
 
